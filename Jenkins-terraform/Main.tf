0a36458649052a4296c5b5f90019a54817ef852e
Abhinav Jain <78918407+Revenant1913@users.noreply.github.com>
2024-03-21 00:52:31 +0530

    Update Main.tf

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -59,7 +59,7 @@ resource "aws_security_group" "Jenkins-sg" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = "ami-09c8d5d747253fb7a"
+  ami                    = "ami-0df4b2961410d4cff"
   instance_type          = "t2.large"
   key_name               = "Abhi"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
